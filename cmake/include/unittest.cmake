a86e78d2126227843caf9830e59afb5bc28e8194
Philippe Coval <philippe.coval@silabs.com>
2025-06-05 17:00:29 +0200

    WIP: mod: cmake/include/unittest.cmake (main)
    Signed-off-by: Philippe Coval <philippe.coval@silabs.com>

--- cmake/include/unittest.cmake
+++ cmake/include/unittest.cmake
@@ -7,6 +7,7 @@ find_program(MEMORYCHECK_COMMAND valgrind)
 set(MEMORYCHECK_COMMAND_OPTIONS
     "--trace-children=yes --leak-check=full --show-leak-kinds=definite,possible"
 )
+set_property(GLOBAL PROPERTY CTEST_TARGET_MEMCHECK "ON")
 
 include(CTest)
 set(CTEST_OUTPUT_ON_FAILURE ON)
